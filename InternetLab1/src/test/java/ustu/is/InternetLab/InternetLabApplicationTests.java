6db5146cad8c94bca0aad7d07d9c45ab02f340c4
Alina5757 <91009972+Alina5757@users.noreply.github.com>
2022-05-19 16:20:52 +0400

    l1 v1
    l1 v1

---
+++ InternetLab1/src/test/java/ustu/is/InternetLab/InternetLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ustu.is.InternetLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
